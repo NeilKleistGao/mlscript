9619b9c1249636ecd8575087cbdb84f9e6c30e5e
Lionel Parreaux <lionel.parreaux@gmail.com>
2026-06-04 18:47:11 +0800

    Minor

--- hkmc2/shared/src/main/scala/hkmc2/codegen/wasm/text/WatBuilder.scala
+++ hkmc2/shared/src/main/scala/hkmc2/codegen/wasm/text/WatBuilder.scala
@@ -926,8 +926,7 @@ class WatBuilder(using TraceLogger, State) extends CodeBuilder:
 
     def splitSuperTail(block: Block): Opt[Block -> Ls[Arg]] = block match
       case End(_) => N
-      case Assign(lhs, Call(Value.SimpleRef(bs: BuiltinSymbol), argss), _: End)
-        if (lhs is State.noSymbol) && (bs is State.superSymbol)
+      case Assign(_: NoSymbol, Call(Value.SimpleRef(State.superSymbol), argss), _: End)
       =>
         S(End("") -> argss.flatten)
       case b: NonBlockTail =>
