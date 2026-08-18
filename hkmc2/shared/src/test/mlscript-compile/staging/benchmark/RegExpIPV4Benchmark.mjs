f786c75d511a788add3a4ec1f4d18b28d477ae24
NeilKleistGao <neilkleist@gmail.com>
2026-08-18 15:53:15 +0800

    Add benchmark

--- hkmc2/shared/src/test/mlscript-compile/staging/benchmark/RegExpIPV4Benchmark.mjs
+++ hkmc2/shared/src/test/mlscript-compile/staging/benchmark/RegExpIPV4Benchmark.mjs
@@ -4,6 +4,7 @@ import { run, bench, boxplot, summary } from 'mitata';
 import SimpleRegExp from "../../SimpleRegExp.mjs"
 import StagedRegExp from "../out/StagedRegExp.mjs"
 import SpecialRegExpIPv4 from "../out/SpecialRegExpIPv4.mjs"
+import TrickRegExpIPv4 from "../out/TrickRegExpIPv4.mjs"
 
 let text = fs.readFileSync("./input-text.txt", "utf8")
 
@@ -19,6 +20,10 @@ boxplot(() => {
 
     bench('SpecialRegExp.matchAllIPv4', () => {
       SpecialRegExpIPv4.matchAllIPv4(text);
+    });
+
+    bench('TrickRegExpIPv4.matchAllIPv4', () => {
+      TrickRegExpIPv4.matchAllIPv4(text);
     });
   });
 });
